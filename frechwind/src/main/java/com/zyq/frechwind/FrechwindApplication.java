51cce8b5061e38c877817dd0e8a54ab0860f6bd7
zhu-yuquan <zhu_yuquan6@163.com>
2020-03-23 23:46:33 +0800

    1

---
+++ frechwind/src/main/java/com/zyq/frechwind/FrechwindApplication.java
@@ -0,0 +1,13 @@
+package com.zyq.frechwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrechwindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrechwindApplication.class, args);
+    }
+
+}
---
+++ frechwind/src/test/java/com/zyq/frechwind/FrechwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyq.frechwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrechwindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
